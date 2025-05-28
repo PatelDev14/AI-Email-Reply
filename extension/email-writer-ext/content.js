bcb78854b343fcdd9e9cb45b43925dc889c73f6f
PatelDev14 <devp1400@gmail.com>
2025-05-28 18:53:08 -0400

    Removed the permissions

--- extension/email-writer-ext/content.js
+++ extension/email-writer-ext/content.js
@@ -145,7 +145,9 @@ function injectButton(){
 
             const emailContent = getEmailContent();
             const selectedTone = dropdown.value;
-            const response = await fetch('http://localhost:8080/api/email/generate',{
+            //const response = await fetch('http://localhost:8080/api/email/generate',{
+            const response = await fetch('https://ai-email-reply-s2aj.onrender.com/api/email/generate',{
+
                 method:'POST',
                 headers: {
                     'Content-Type': 'application/json',
